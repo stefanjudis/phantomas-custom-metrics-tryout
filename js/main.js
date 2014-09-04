ebb40497bf25db01f008163694fb9fe71562ac49
stefan judis <stefanjudis@gmail.com>
2014-09-04 23:42:26 +0200

    modified addOffender call

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ if ( typeof window.__phantomas !== 'undefined' ) {
     var foo = 2222;
 
     if ( foo > 1234 ) {
-      phantomas.addOffender( 'foo', 'Value higher than expected in ' + foo );
+      phantomas.addOffender( 'foo', 'Value higher than expected in ' + foo, foo );
     }
   } )( window.__phantomas );
 }
