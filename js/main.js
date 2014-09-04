c64378ec2d3044f899e5fe65063adb7085825ad1
stefan judis <stefanjudis@gmail.com>
2014-09-04 23:38:14 +0200

    fixed typo in helper

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-if ( window__phantomas ) {
+if ( typeof window.__phantomas !== 'undefined' ) {
   ( function( phantomas ) {
     if ( 2222 > 1234 ) {
       phantomas.addOffender( 'foo', 'Value higher than expected in jooo' );
