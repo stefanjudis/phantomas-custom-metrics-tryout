6c632bf7db052a7e8daad633d5e0e744bf4d59bc
stefan judis <stefanjudis@gmail.com>
2014-09-04 23:36:01 +0200

    added first phantomas helper tryout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+if ( window__phantomas ) {
+  ( function( phantomas ) {
+    if ( 2222 > 1234 ) {
+      phantomas.addOffender( 'foo', 'Value higher than expected in jooo' );
+    }
+  } )( window.__phantomas );
+}
