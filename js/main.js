7bb03d48b1e70375266e0dccb13031e7a2df7c54
stefan judis <stefanjudis@gmail.com>
2014-09-05 01:55:48 +0200

    playin

--- js/main.js
+++ js/main.js
@@ -1,33 +1,47 @@
+// check if phantomas is available
+// -> this means page is visited by phantomas
 if ( typeof window.__phantomas !== 'undefined' ) {
   ( function( phantomas ) {
-    var foo = 2222;
 
-    phantomas.setMetric( 'loooooooopTime' );
-    phantomas.setMetric( 'functionCall' );
+    // add offenders for 'ourReadyMetric'
+    phantomas.addOffender( 'ourReadyMetric', 'Something happened related to ourReadyMetric : someValue foo' );
+    phantomas.addOffender( 'ourReadyMetric', 'Something happened related to ourReadyMetric : someValue bar' );
+    phantomas.addOffender( 'ourReadyMetric', 'Something happened related to ourReadyMetric : someValue baz' );
 
-    if ( foo > 1234 ) {
-      phantomas.addOffender( 'loooooooopTime', 'Value higher than expected in ' + foo );
-      ++foo;
-      phantomas.addOffender( 'loooooooopTime', 'Value higher than expected in ' + foo );
-      ++foo;
-      phantomas.addOffender( 'loooooooopTime', 'Value higher than expected in ' + foo );
-    }
-
-
+    // set up start value
+    // to calculate a time difference
     var start = Date.now();
 
-    setTimeout( function() {
-      phantomas.setMetric( 'loooooooopTime', Date.now() - start );
-    }, 500 );
+    // set up our listener for DOMready
+    // to wait for something
+    document.addEventListener( 'DOMContentLoaded', function() {
+      phantomas.setMetric( 'ourReadyMetric', Date.now() - start );
+    }, false );
 
+
+    /**
+     * Define metric for counting function call of a.foo
+     */
+    phantomas.setMetric( 'functionCalls' );
+
+    /**
+     * Dummy object we want to spy
+     * @type {Object}
+     */
     var a = {
+      /**
+       * Function we want to spy
+       */
       foo : function() {}
     }
 
+    // set up spy for a.foo
     phantomas.spy( a, 'foo', function() {
-      phantomas.incrMetric( 'functionCall' );
+      // increment value for metric 'functionCalls'
+      phantomas.incrMetric( 'functionCalls' );
     } );
 
+    // execute a.foo 4 times
     a.foo();
     a.foo();
     a.foo();
